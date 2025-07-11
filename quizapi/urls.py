ebf8ef7f1739857b0dd464f76a9741a88c17edd0
jayson-pardilla <pardillajayson004@gmail.com>
2025-07-11 12:31:53 +0800

    Update urls.py for productions

--- quizapi/urls.py
+++ quizapi/urls.py
@@ -16,10 +16,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, re_path, include
 from django.views.static import serve
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), 
+    path('api/', include('quiz.urls')), 
 ]
 
 urlpatterns += [
